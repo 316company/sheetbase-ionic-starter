dd2e384fdca8d5c05e0b8436446381af1e2aad81
Lam Nhan <lamnhan92@gmail.com>
2018-04-18 21:15:22 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -4,19 +4,19 @@ export const CONFIG = {
     *   Authentication, using Google API key for public spreadsheet
     * 
     * */
-    apiKey: '<api_key_here>',
+    apiKey: 'AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU', // replace this with your API Key
 
     /*
     *   Database address, a spreadsheet id
     *
     * */
-    database: '1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY',
+    database: '1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY', // replace this with your database ID
     
     /*
     *   Google script web app address, using as backend
     *
     * */
-    backend: '',
+    backend: '', // replace this with you backend ID
 
     /*
     *   (optional) Custom modifiers, modify the data returned from spreadsheet
