8f694b2132213e6b067bc3918b175180a138b59e
Lam Nhan <lamnhan92@gmail.com>
2018-05-02 03:00:12 +0700

    update

--- src/configs/sheetbase.config.ts
+++ src/configs/sheetbase.config.ts
@@ -1,26 +1,7 @@
 export const SHEETBASE_CONFIG = {
-    // replace this with your own API Key
-    apiKey: 'SUkepreTR52rAhustEju7haYak4ZUhax',    
-    // replace with your database
-    database: '1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY',
-    // backend id
-    backend: 'AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ',
-    
-    // (optional) Google API Key
-    googleApiKey: 'AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU',    
-
-    // (optional) Custom modifiers, modify the data returned from spreadsheet
-    modifiers: {
-        'posts': posts
-    }
-}
-
-
-
-/*
-*   Modifier functions
-* */
-export function posts(item: any): any {
-    item.greeting = 'Hello, ['+ (item.key || item.slug || item.id) +']! :)'; // add the field 'greeting' to every item has 'post' data
-    return item;
+    // Must be JSON-liked format
+    "apiKey": "SUkepreTR52rAhustEju7haYak4ZUhax",
+    "database": "1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY",
+    "backend": "AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ",
+    /*(optional)*/ "googleApiKey": "AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU",    
 }
