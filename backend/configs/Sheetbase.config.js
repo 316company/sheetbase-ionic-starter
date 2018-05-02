87eadaa0745f6e4e94c92d2664ded15231a9dca3
Lam Nhan <lamnhan92@gmail.com>
2018-05-02 15:51:11 +0700

    update

--- backend/configs/Sheetbase.config.js
+++ backend/configs/Sheetbase.config.js
@@ -2,7 +2,6 @@ function SHEETBASE_CONFIG() {
     // Must be JSON-liked format
     return {
         "apiKey": "SUkepreTR52rAhustEju7haYak4ZUhax",
-        "backend": "AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ",
         "database": "1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY",
         "databaseBackend": "1nqNHOBEVWNGb6PECSpn41I73XYmClt0DC67ivQi-7xo",
         "encryptionKey": "3raR?BrUg#dr",           
