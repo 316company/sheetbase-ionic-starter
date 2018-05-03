d14358df999728e8ebb6b14dd0ea85d04a3912b4
Lam Nhan <lamnhan92@gmail.com>
2018-05-04 01:01:08 +0700

    update

--- backend/configs/Sheetbase.config.js
+++ backend/configs/Sheetbase.config.js
@@ -2,7 +2,7 @@ function SHEETBASE_CONFIG() {
     // Must be JSON-liked format
     return {
         "apiKey": "SUkepreTR52rAhustEju7haYak4ZUhax",
-        "database": "1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY",
+        "databaseId": "1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY",
         "encryptionKey": "3raR?BrUg#dr",           
         "contentFolder": "1pyWM0NKP3KMhZI_J_UM26to3CAexgG-d"
     };
--- src/configs/sheetbase.config.ts
+++ src/configs/sheetbase.config.ts
@@ -1,7 +1,5 @@
 export const SHEETBASE_CONFIG = {
     // Must be JSON-liked format
     "apiKey": "SUkepreTR52rAhustEju7haYak4ZUhax",
-    "database": "1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY",
-    "backend": "AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ",
-    /*(optional)*/ "googleApiKey": "AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU",    
+    "backendUrl": "https://script.google.com/macros/s/AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ/exec"   
 }
